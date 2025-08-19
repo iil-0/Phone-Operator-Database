e1df7d0194b238793c3b11e584a4762661fa0176
ilke <117925325+iil-0@users.noreply.github.com>
2025-08-19 11:30:15 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,11 +1,11 @@
 SELECT 
     i.IlIsmi, 
-    COUNT(DISTINCT a.AboneID) AS AboneSayisi     -- Aynı abone birden fazla pakete sahipse abone tekrarlanSmasın.
-FROM ABONE a                                     --Yani artık bu tabloya a. diye erişeceğiz.
-JOIN Adresler adr ON a.AdresID = adr.AdresID     --Her abonenin adresini almak icin abone.Adresid ile adres tablosundaki id yi bagladik. 
-JOIN Iller i ON adr.IlID = i.IlID                --Adres tablosundaki ilid ile il tablosunun id lerini bagladık.Il e erismeye calisiyoruz.
-JOIN Sozlesmeler s ON s.AboneID = a.AboneID      --Abonenin idsi ile sözlesmelerdeki aboneid yi bagladık.Abonelerin yaptıgı sozlesmelere erismek icin.
-JOIN SozlesmePaketleri sp ON sp.SozlesmeID = s.SozlesmeID -- Her sözleşmenin içindeki paket bilgilerine erişmek için SozlesmePaketleri tablosuna bağlanıyoruz.
+    COUNT(DISTINCT a.AboneID) AS AboneSayisi     
+FROM ABONE a                                     
+JOIN Adresler adr ON a.AdresID = adr.AdresID     
+JOIN Iller i ON adr.IlID = i.IlID                
+JOIN Sozlesmeler s ON s.AboneID = a.AboneID      
+JOIN SozlesmePaketleri sp ON sp.SozlesmeID = s.SozlesmeID 
 WHERE 
     a.AboneID IN (
         SELECT a2.AboneID
@@ -43,23 +43,23 @@ ORDER BY
         ELSE NULL
     END DESC
 
-
+-- Diger select sorgusu.
 SELECT 
-    a.AboneID,  -- Abonenin ID'si
-    s.SozlesmeID,  -- Aktif sözleşme ID
-    s.TarifeIsmi,  -- Şu anki paketin ismi
-    s.TarifeKalanInternetMB  -- Şu anki pakette kalan internet miktarı
+    a.AboneID,  
+    s.SozlesmeID,  
+    s.TarifeIsmi,  
+    s.TarifeKalanInternetMB  
 FROM ABONE a
 JOIN Sozlesmeler s ON s.AboneID = a.AboneID
-WHERE s.BitisTarihi > GETDATE()  -- Sadece şu an aktif olan sözleşmeleri al
-AND NOT EXISTS (  -- Eğer aşağıdaki 3 popüler paketin hepsini kullanmamışsa, dışla
+WHERE s.BitisTarihi > GETDATE()  
+AND NOT EXISTS (  
     SELECT TOP 1 1
     FROM (
         SELECT TOP 3 sp2.TarifePaketID
         FROM ABONE a2
         JOIN Sozlesmeler s2 ON s2.AboneID = a2.AboneID
         JOIN SozlesmePaketleri sp2 ON sp2.SozlesmeID = s2.SozlesmeID
-        WHERE ABS(DATEDIFF(YEAR, a.DogumTarihi, a2.DogumTarihi)) <= 3  -- Yaş farkı ±3 yıl
+        WHERE ABS(DATEDIFF(YEAR, a.DogumTarihi, a2.DogumTarihi)) <= 3  
         GROUP BY sp2.TarifePaketID
         ORDER BY COUNT(*) DESC
     ) AS EnPopuler
@@ -68,6 +68,7 @@ AND NOT EXISTS (  -- Eğer aşağıdaki 3 popüler paketin hepsini kullanmamış
         FROM Sozlesmeler s3
         JOIN SozlesmePaketleri sp3 ON sp3.SozlesmeID = s3.SozlesmeID
         WHERE s3.AboneID = a.AboneID
-        AND sp3.TarifePaketID = EnPopuler.TarifePaketID  -- Bu 3 paketten birini bile kullanmamışsa dışla
+        AND sp3.TarifePaketID = EnPopuler.TarifePaketID  
     )
 )
+
